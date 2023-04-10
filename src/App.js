983908e0cb781165313af421dc0a7360597cfeea
Mounika <mounikasantoshi99@gmail.com>
2023-04-10 22:27:55 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-// import Employee from './components/Employee';
-// import MyComponent from './components/MyComponent';
 import Search from './components/search';
 
 function App() {
   return (
     <div >
      <Search/>
-     {/* <Employee/> */}
-     {/* <MyComponent/> */}
     </div>
   );
 }
--- src/components/MyComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MyComponent() {
-  return (
-    <div>Hello World</div>
-  )
-}
-
-export default MyComponent
